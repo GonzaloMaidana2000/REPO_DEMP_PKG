612c3c4a1e8d0f9866b40a959b4017d6c243be44
Gonzalo Daniel <gonzalomaidana2000@gmail.com>
2025-06-23 16:35:50 -0300

    first commit

---
+++ migrations/V001_20250623161152__Usuario.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE PACKAGE BODY DEMO_PKG AS
+  PROCEDURE saludar IS
+  BEGIN
+    DBMS_OUTPUT.PUT_LINE('Versión 1.0 - GALI-1001');
+	DBMS_OUTPUT.PUT_LINE('Versión 2.0 - GALI-627');
+	DBMS_OUTPUT.PUT_LINE('Versión 2.0 - Despliegue con Flyway y Github');
+    DBMS_OUTPUT.PUT_LINE('ESTA ES UNA NUEVA INTEGRACION EN AMBIENTES');
+    DBMS_OUTPUT.PUT_LINE('****************************************************'); 
+    DBMS_OUTPUT.PUT_LINE('#################################################-----###'); 
+    DBMS_OUTPUT.PUT_LINE('----------------------------------------------------'); 
+
+
+  END saludar;
+END DEMO_PKG;
